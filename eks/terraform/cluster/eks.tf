1d97c4c1c50d448ad484b9d3db40444e5f77f4a0
Jafar <jafer.ba@gmail.com>
2022-07-22 11:42:55 +0300

    restore proper configs

--- eks/terraform/cluster/eks.tf
+++ eks/terraform/cluster/eks.tf
@@ -137,9 +137,9 @@ resource "aws_eks_node_group" "nodegroup" {
   ami_type = "AL2_x86_64_GPU"
   capacity_type = "ON_DEMAND"
   scaling_config {
-    desired_size = 1
+    desired_size = 3
     max_size     = 4
-    min_size     = 0
+    min_size     = 2
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
