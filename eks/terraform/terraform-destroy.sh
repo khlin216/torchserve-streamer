683f1e4472608eb337a85ecd25d6f34b25f06d98
streamsnipersgames <streamsnipersgames@gmail.com>
2022-07-22 05:39:30 -0400

    make wsl-friendly

--- eks/terraform/terraform-destroy.sh
+++ eks/terraform/terraform-destroy.sh
@@ -1,10 +1,8 @@
-function run_destroy () {
-    
-    terraform destroy -auto-approve
-}
 export KUBE_CONFIG_PATH=~/.kube/config # IF YOU ARE USING WINDOWS THIS SHOULD BE DIFFERENT
 
 ## DONT RUN THIS IN DEV ONLY IN PROD
-cd helm && run_destroy
-cd ../cluster && run_destroy
+cd helm
+terraform destroy -auto-approve
 
+cd ../cluster
+terraform destroy -auto-approve
