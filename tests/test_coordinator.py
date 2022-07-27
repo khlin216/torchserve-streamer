f45ada60e80a2e389b9d5a9d2b622be400276f1f
Jafar <jafer.ba@gmail.com>
2022-07-27 23:59:54 +0300

    add unittests

---
+++ tests/test_coordinator.py
@@ -0,0 +1,49 @@
+import sys
+
+sys.path.append("../torchserve")
+from  coordinator import MMdetHandler
+import unittest
+import time
+
+class Test_Coordinator(unittest.TestCase):
+    def __init__(self, methodName="coordinator") -> None:
+
+        class Metrics:
+            def add_time(self, *args, **kwargs):
+                self.log(*args, **kwargs)
+            def add_metric(self, *args, **kwargs):
+                self.log(*args, **kwargs)
+            def log(self, *args, **kwargs):
+                print("Logger.args= ", *args)
+                print("Logger.kwargs= ", *args)    
+
+        class Context:
+            system_properties = {
+                "gpu_id" : "0"
+            }
+
+            metrics = Metrics()
+            def get_request_header(self, *args, **kwargs):
+                return True
+
+
+        super().__init__(methodName)
+        self.model_handler = MMdetHandler()
+        self.model_handler.initialize(context=Context())
+        self.handle = self.model_handler.handle
+        self.context = Context()
+
+    def test_batch(self, batch_sz=128, img_path = "../tests/influencer.png"):
+        img_binary = open(img_path, "rb").read()
+        json_req = {"body" : img_binary}
+        batch = [json_req for _ in range(batch_sz)]
+        print("Initializing")
+        def handle_once():
+            tic = time.time()
+            self.handle(batch, self.context)
+            toc = time.time()
+            print("HandleTime", toc - tic)
+        for _ in range(1):
+            handle_once()
+if __name__ == "__main__":
+    unittest.main()
