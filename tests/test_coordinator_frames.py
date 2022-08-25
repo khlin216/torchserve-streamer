cd53023565eb426e63dce0535ea9209c3077b030
streamsnipersgames <streamsnipersgames@gmail.com>
2022-08-25 12:23:21 -0400

    draw dots on img

--- tests/test_coordinator_frames.py
+++ tests/test_coordinator_frames.py
@@ -10,8 +10,6 @@ import mmcv
 import cv2
 
 
-
-
 from coordinator import TriangleHandler
 
 
@@ -36,25 +34,25 @@ if __name__ == "__main__":
         def __init__(self, *args, **kwargs) -> None:
             self.system_properties = {"gpu_id": "0"}    
             self.metrics = Metrics()
+
         def __str__(self):
             return ("here")
 
         def get_request_header(self, *args):
             return False
+
     print("tmp", Temp())
     handler = TriangleHandler().initialize(Temp())
     import os
     for file in os.listdir("../images"):
         file_ = os.path.join("../images", file)
-        
-        img = open(file_,"rb").read()
-        # img = open("../apex_apex1 (10).png", "rb").read()
-        cvimg = mmcv.imfrombytes(img)# [:,:,::-1] # opencv bgr to rgb
+        cvimg = cv2.imread(file_)
+        cvimg = cv2.cvtColor(cvimg, cv2.COLOR_BGR2RGB)
         cvimg = cv2.resize(cvimg, (640, 640), interpolation=cv2.INTER_NEAREST)
         print(cvimg.shape)
         tic = time.time()
         
-        results = handler.handle([{"data" : cvimg.tobytes()}], Temp())
+        results = handler.handle([{"data": cvimg.tobytes()}], Temp())
         pilim = Image.fromarray(cvimg)
         print(file_, results)
         for res in results[0]["triangles"]:
@@ -72,8 +70,12 @@ if __name__ == "__main__":
             color = tuple((np.random.random(size=3) * 256).astype(int))
             print(color)
             print(res)
-            draw.rectangle((xs[0], ys[0], xs[1], ys[1]), outline=color,width=5)
+            draw.rectangle((xs[0], ys[0], xs[1], ys[1]), outline=color, width=2)
             draw.text((xs[0]+10, ys[0]+10), f"Conf={round(res['yolo_confidence'],2)}", fill="blue")
-        
+            xs, ys = [], []
+            for vert in res["vertices"]:
+                x, y = vert["x"], vert["y"]
+                draw.ellipse((x-3, y-3, x+3, y+3), "green", width=5)
+
         os.makedirs("./deleteme/frames/",exist_ok=True)
         pilim.save(f"./deleteme/{file}")
