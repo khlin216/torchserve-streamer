ee731de9e95a4e36444c9cbd504cbe085a62f6d0
streamsnipersgames <streamsnipersgames@gmail.com>
2022-08-25 10:36:55 -0400

    minor cosmetic changes

--- torchserve/methods/misc.py
+++ torchserve/methods/misc.py
@@ -1,7 +1,7 @@
-
 import torch.nn.functional as F
 import torch
 import warnings
+
 
 class CoordinatesTranslator:
     def __init__(self, x0, y0, x1, y1, img_index, triangle_index, device) -> None:
@@ -21,25 +21,19 @@ class CoordinatesTranslator:
         self.triangle_index = triangle_index
         self.tri = (x1 - x0, y1 - y0)
 
-
-    def is_positive_area(self, x0, y0, x1, y1):
-        
-        return  x1 > x0 and y1 > y0
-
+    @staticmethod
+    def is_positive_area(x0, y0, x1, y1):
+        return x1 > x0 and y1 > y0
 
     def crop_triangle(self, img):
-
         img = img.to(self.device)
         if list(img.shape) != [3, 640, 640]:
             raise KeyError(f"img shape should be (640, 640), 3 but found {list(img.shape)}")
         x0, y0, x1, y1 = self.coords
-        triangle_ = img[:,y0:y1, x0:x1]#.permute(1, 2, 0)
+        triangle_ = img[:,y0:y1, x0:x1]  #.permute(1, 2, 0)
         triangle_ = triangle_.reshape(1, *triangle_.shape)
-        triangle_ = F.interpolate(triangle_,(64,64))[0].permute(1,2,0)
-
+        triangle_ = F.interpolate(triangle_, (64, 64))[0].permute(1, 2, 0)
         return triangle_
-    
-
 
     def translate_triangle_to_image(self, x_, y_):
         """
@@ -47,17 +41,16 @@ class CoordinatesTranslator:
         """ 
         x0, y0, x1, y1 = self.coords
         if x_ < 0 or x_ > self.resize_shape[0] or y_ > self.resize_shape[1] or y_ < 0:
-            
+
             warnings.warn("vod_triangles returning vertex out of the original triangle??"
                 f"Condition is {x_} < 0 or {x_} > {self.resize_shape[0]} or {y_} > {self.resize_shape[1]} or {y_} < 0")
-            
+
         return (
                 (x_ * self.tri[0] / self.resize_shape[0]) + x0, 
                 (y_ * self.tri[1] / self.resize_shape[1]) + y0
             )
 
     def translate_x_to_img(self, x_):
-        
         return self.translate_triangle_to_image(x_, 0)[0]
 
     def translate_y_to_img(self, y_):
@@ -71,15 +64,13 @@ class CoordinatesTranslator:
 
 
 def fetch_triangles_translators_batches(yolo_output, imgs, n_batch, device):
-
     # cut triangle
     imgs_triangles = []
     translators = []
     for img_num in range(len(yolo_output)):
         for tr_num in range(len(yolo_output[img_num])):
-
             translator = CoordinatesTranslator(
-                *yolo_output[img_num][tr_num][:4], 
+                *yolo_output[img_num][tr_num][:4],
                 img_index=img_num,
                 triangle_index=tr_num, 
                 device=device
